44ea98985847ecd6e202091fc4b83d19a2b5cdd0
Aman Shetty <shettyaman016@gmail.com>
2025-03-12 23:53:16 +0530

    🎉 Project Setup

---
+++ src/navigation/AppNavigator.jsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Login from '../screens/Login';
+
+const Stack = createNativeStackNavigator();
+
+const AppNavigator = () => {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name='login' component={Login}/>
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
+
+export default AppNavigator;
